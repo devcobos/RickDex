ffba47ed85a3b182fcb2336d368520b8f8b0af05
Joaquín León <joaquinleoncobos@gmail.com>
2024-02-20 21:35:11 +0100

    Minor changes to list characters

--- src/app/modules/characters/components/list/list-characters.component.ts
+++ src/app/modules/characters/components/list/list-characters.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { Character, ListCharacterResponse } from 'src/app/core/interfaces/character.interfaces';
 import { CharacterService } from 'src/app/core/services/character.service';
 import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/overlay';
@@ -16,11 +16,21 @@ export class ListCharactersComponent implements OnInit, OnDestroy {
 
   private scrollSubscription!: Subscription;
 
-  characters: Character[] = [];
+  protected characters: Character[] = [];
   private currentPage = 1;
 
   ngOnInit(): void {
     this.loadCharacters();
+    this.observingScroll();
+  }
+
+  ngOnDestroy(): void {
+    if (this.scrollSubscription) {
+      this.scrollSubscription.unsubscribe();
+    }
+  }
+
+  private observingScroll(): void {
     this.scrollSubscription = this._scrollDispatcher.scrolled().subscribe((scrollable: void | CdkScrollable) => {
       if (scrollable) {
         const viewport = scrollable.measureScrollOffset('bottom');
@@ -31,17 +41,14 @@ export class ListCharactersComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    if (this.scrollSubscription) {
-      this.scrollSubscription.unsubscribe();
-    }
-  }
-
   private loadCharacters(): void {
-    if (this.currentPage === 42) return;
+    // TODO Esto es una chapuza. !No se muestran los ultimos personajes
+    if (this.currentPage === 43) {
+      return;
+    }
 
     this._characterSevice.listCharacters(this.currentPage).subscribe((data: ListCharacterResponse) => {
-      this.characters.push(...data.results);
+      this.characters = [...this.characters, ...data.results];
       this.currentPage++;
     });
 
