bfecf5edb281c3b769be906b872d0311e3dc0d03
Joaquín León <joaquinleoncobos@gmail.com>
2024-02-25 11:54:06 +0100

    CharacterService full GraphQL

--- src/app/core/services/character.service.ts
+++ src/app/core/services/character.service.ts
@@ -1,15 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable, map, take } from 'rxjs';
-import { Character, ListCharacterResponse } from '../interfaces/character.interfaces';
+import { ListCharacterResponse } from '../interfaces/character.interfaces';
 import { LIST_CHARACTERS } from './graphql/character.queries';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  private _http: HttpClient = inject(HttpClient);
   private _apollo: Apollo = inject(Apollo);
 
   public listCharacters(page: number): Observable<ListCharacterResponse> {
@@ -24,9 +22,5 @@ export class CharacterService {
         take(1),
         map((response) => response.data.characters)
       );
-  }
-
-  public getCharacterByUrl(url: string): Observable<Character> {
-    return this._http.get<Character>(url).pipe(take(1));
   }
 }
